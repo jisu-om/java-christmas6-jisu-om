8a5315fade51bf757a8a4f03e53d905c91d1c6e9
jisu-om <lifyji@naver.com>
2023-11-11 11:23:12 +0900

    Refactor(discountAmount): 하드코딩된 부분 리팩토링

--- src/main/java/christmas/domain/DiscountAmount.java
+++ src/main/java/christmas/domain/DiscountAmount.java
@@ -1,13 +1,16 @@
 package christmas.domain;
 
+import christmas.constants.DateConstants;
+
 import java.util.function.BiFunction;
 
+import static christmas.constants.DateConstants.*;
 import static christmas.constants.DiscountConstants.*;
 import static christmas.domain.MenuCategory.DESSERT;
 import static christmas.domain.MenuCategory.MAIN;
 
 public enum DiscountAmount {
-    CHRISTMAS_D_DAY((orders, date) -> BASE_DISCOUNT + (date.getDate() - 1) * CHRISTMAS_RATE),
+    CHRISTMAS_D_DAY((orders, date) -> BASE_DISCOUNT + calculatePassedDays(date) * CHRISTMAS_RATE),
     WEEKDAYS((orders, date) -> (orders.countOrderItemByCategory(DESSERT) * DAILY_RATE)),
     WEEKENDS((orders, date) -> (orders.countOrderItemByCategory(MAIN) * DAILY_RATE)),
     SPECIAL((orders, date) -> BASE_DISCOUNT),
@@ -20,7 +23,12 @@ public enum DiscountAmount {
         this.discountCalculator = discountCalculator;
     }
 
+    private static long calculatePassedDays(VisitingDate date) {
+        return date.getDate() - EVENT_START;
+    }
+
     public long calculateDiscountAmount(Orders orders, VisitingDate date) {
         return discountCalculator.apply(orders, date);
     }
+
 }
