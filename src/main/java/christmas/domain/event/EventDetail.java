bda4060ec85db022b55aab9d60326446ff2ef9a8
jisu-om <lifyji@naver.com>
2023-11-14 00:17:14 +0900

    Refactor(eventDetail): 날짜조건(dateCondition) 표현 방식 변경

--- src/main/java/christmas/domain/event/EventDetail.java
+++ src/main/java/christmas/domain/event/EventDetail.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
 import static christmas.constants.DateConstants.*;
@@ -15,53 +16,44 @@ import static christmas.domain.menu.MenuCategory.DESSERT;
 import static christmas.domain.menu.MenuCategory.MAIN;
 
 public enum EventDetail {
-    //TODO 날짜 조건 규칙 생성
     CHRISTMAS_D_DAY(
             "크리스마스 디데이 할인",
-            IntStream.rangeClosed(EVENT_START, CHRISTMAS_EVENT_END)
-                    .boxed()
-                    .toList(),
+            date -> date >= EVENT_START && date <= CHRISTMAS_EVENT_END,
             BASE_PRICE_CONDITION,
             orders -> true,
             (date, orders) -> BASE_DISCOUNT + calculatePassedDays(date) * CHRISTMAS_RATE),
     WEEKDAYS(
             "평일 할인",
-            IntStream.rangeClosed(EVENT_START, EVENT_END)
-                    .filter(i -> i % 7 >= 3 || i % 7 == 0).boxed()
-                    .toList(),
+            date -> date >= EVENT_START && date <= CHRISTMAS_EVENT_END && (date % 7 >= 3 || date % 7 == 0),
             BASE_PRICE_CONDITION,
             orders -> orders.existsOrderItemByCategory(DESSERT),
             (date, orders) -> orders.countOrderItemByCategory(DESSERT) * DAILY_RATE),
     WEEKENDS(
             "주말 할인",
-            IntStream.rangeClosed(EVENT_START, EVENT_END)
-                    .filter(i -> i % 7 == 1 || i % 7 == 2).boxed()
-                    .toList(),
+            date -> date >= EVENT_START && date <= EVENT_END && (date % 7 == 1 || date % 7 == 2),
             BASE_PRICE_CONDITION,
             orders -> orders.existsOrderItemByCategory(MAIN),
             (date, orders) -> orders.countOrderItemByCategory(MAIN) * DAILY_RATE),
     SPECIAL(
             "특별 할인",
-            List.of(3, 10, 17, 24, 25, 31),
+            List.of(3, 10, 17, 24, 25, 31)::contains,
             BASE_PRICE_CONDITION,
             orders -> true,
             (date, orders) -> BASE_DISCOUNT),
     GIVE_AWAY(
             "증정 이벤트",
-            IntStream.rangeClosed(EVENT_START, EVENT_END)
-                    .boxed()
-                    .toList(),
+            date -> date >= EVENT_START && date <= EVENT_END,
             GIVE_AWAY_PRICE_CONDITION,
             orders -> true,
             (date, orders) -> GIVE_AWAY_PRICE);
 
     private final String eventName;
-    private final List<Integer> dateCondition;
+    private final Predicate<Integer> dateCondition;
     private final long priceCondition;
     private final Function<Orders, Boolean> itemCondition;
     private final BiFunction<VisitingDate, Orders, Long> benefitCalculator;
 
-    EventDetail(String eventName, List<Integer> dateCondition, long priceCondition,
+    EventDetail(String eventName, Predicate<Integer> dateCondition, long priceCondition,
                 Function<Orders, Boolean> itemCondition,
                 BiFunction<VisitingDate, Orders, Long> benefitCalculator) {
         this.eventName = eventName;
@@ -77,7 +69,7 @@ public enum EventDetail {
 
     public static List<EventDetail> findByCondition(VisitingDate date, Orders orders) {
         return Arrays.stream(EventDetail.values())
-                .filter(condition -> condition.dateCondition.contains(date.getDate()))
+                .filter(condition -> condition.dateCondition.test(date.getDate()))
                 .filter(condition -> orders.calculateOriginalTotalAmount() >= condition.priceCondition)
                 .filter(condition -> condition.itemCondition.apply(orders))
                 .toList();
