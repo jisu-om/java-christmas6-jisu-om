e14a14cddc06e4ab9614495dadcf55c531f9fed4
jisu-om <lifyji@naver.com>
2023-11-15 02:29:24 +0900

    Test(menuItemTest): 테스트코드 작성

---
+++ src/test/java/christmas/domain/menu/MenuItemTest.java
@@ -0,0 +1,15 @@
+package christmas.domain.menu;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class MenuItemTest {
+    @DisplayName("findByName 테스트")
+    @Test
+    void findByName() {
+        MenuItem item = MenuItem.findByName("타파스");
+        assertThat(item.getCategory()).isEqualTo(MenuCategory.APPETIZER);
+    }
+}
