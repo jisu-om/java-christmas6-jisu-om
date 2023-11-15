5fcf9f64c1b04c17cf81a46c3cab857ff462e37c
jisu-om <lifyji@naver.com>
2023-11-15 20:15:49 +0900

    Test(visitingDateInputHandlerTest): 테스트코드 작성

---
+++ src/test/java/christmas/controller/VisitingDateInputHandlerTest.java
@@ -0,0 +1,34 @@
+package christmas.controller;
+
+
+import christmas.domain.orders.Orders;
+import christmas.domain.visitingDate.VisitingDate;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class VisitingDateInputHandlerTest {
+    @DisplayName("createVisitingDate 정상 테스트")
+    @Test
+    void createVisitingDate_o() {
+        // given
+        VisitingDateInputHandler visitingDateInputHandler = VisitingDateInputHandler.of(InputView.getInstance(), OutputView.getInstance());
+        command("30");
+
+        // when
+        VisitingDate date = visitingDateInputHandler.createVisitingDate();
+
+        // then
+        assertThat(date).isNotNull();
+    }
+
+    private void command(final String... args) {
+        final byte[] buf = String.join("\n", args).getBytes();
+        System.setIn(new ByteArrayInputStream(buf));
+    }
+}
