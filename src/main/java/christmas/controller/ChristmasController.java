3ed8499a69e077c32438af127aac2b9e5fdd2a8b
jisu-om <lifyji@naver.com>
2023-11-17 21:05:16 +0900

    Refactor: inputHandler 삭제하고 supplier 사용해서 입력값 받아옴

--- src/main/java/christmas/controller/ChristmasController.java
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -1,18 +1,21 @@
 package christmas.controller;
 
+import christmas.domain.orders.OrderItem;
 import christmas.domain.orders.Orders;
 import christmas.domain.visitingDate.VisitingDate;
+import christmas.dto.OrderItemDto;
 import christmas.dto.OrdersDto;
 import christmas.dto.ResultDto;
 import christmas.service.ChristmasManager;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
+import java.util.List;
+import java.util.function.Supplier;
+
 public class ChristmasController {
     private final InputView inputView;
     private final OutputView outputView;
-    private VisitingDateInputHandler visitingDateInputHandler;
-    private OrdersInputHandler ordersInputHandler;
     private ChristmasManager christmasManager;
 
     private ChristmasController(InputView inputView, OutputView outputView) {
@@ -25,23 +28,43 @@ public class ChristmasController {
     }
 
     public void run() {
-        initializeInputHandlers();
         initializeChristmasManager();
         printOrders();
         printResult();
     }
 
-    private void initializeInputHandlers() {
-        visitingDateInputHandler = VisitingDateInputHandler.of(inputView, outputView);
-        ordersInputHandler = OrdersInputHandler.of(inputView, outputView);
-    }
-
     private void initializeChristmasManager() {
         outputView.printStart();
-        VisitingDate date = visitingDateInputHandler.createVisitingDate();
-        Orders orders = ordersInputHandler.createOrders();
+        VisitingDate date = createVisitingDate();
+        Orders orders = createOrders();
         christmasManager = ChristmasManager.of(date, orders);
         outputView.printResultStart(date.provideDate());
+    }
+
+    private VisitingDate createVisitingDate() {
+        return readUserInput(() -> {
+            int input = inputView.readVisitingDate();
+            return VisitingDate.from(input);
+        });
+    }
+
+    private Orders createOrders() {
+        return readUserInput(() -> {
+            List<OrderItem> items = inputView.readOrders().stream()
+                    .map(OrderItemDto::toOrderItem)
+                    .toList();
+            return Orders.from(items);
+        });
+    }
+
+    private <T> T readUserInput(Supplier<T> supplier) {
+        while (true) {
+            try {
+                supplier.get();
+            } catch (IllegalArgumentException e) {
+                outputView.printError(e.getMessage());
+            }
+        }
     }
 
     private void printOrders() {
